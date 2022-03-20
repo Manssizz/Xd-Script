d0c28efb50720af0ef1d85171279983f2ebafc1d
phreaker56 <92488891+phreaker56@users.noreply.github.com>
2022-03-19 19:17:00 -0600

    Update gerar.sh

--- BOT/gerador/gerar.sh
+++ BOT/gerador/gerar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 clear
 [[ -d /etc/http-shell ]] && mkdir /bin/ejecutar 
-[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/v-new.log https://www.dropbox.com/s/s51xxefw5zrxmbo/v-new.log && chmod 777 v-new.log
-[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/echo-ram.sh https://www.dropbox.com/s/w61gsvlryl30enl/echo-ram.sh && chmod 777 echo-ram.sh
-[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/raml https://www.dropbox.com/s/v6e68tc8rk6a320/raml && chmod 777 raml
+[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/v-new.log https://www.dropbox.com/s/s51xxefw5zrxmbo/v-new.log
+[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/echo-ram.sh https://www.dropbox.com/s/w61gsvlryl30enl/echo-ram.sh && sudo chmod 777 echo-ram.sh
+[[ -d /bin/ejecutar ]] && wget -q -O /bin/ejecutar/raml https://www.dropbox.com/s/v6e68tc8rk6a320/raml
 rm -f /etc/newadm-instalacao
 [[ -e /etc/newadm-instalacao ]] && BASICINST="$(cat /etc/newadm-instalacao)" || BASICINST="cabecalho menu_credito ferramentas limitera menu_inst PPub.py usercodes ferramentascodes limiterb payloads ssl limiter.sh paysnd.sh trans verifica PDirect.py v-local.log idioma_geral PGet.py ultrahost menu optimizador POpen.py shadowsocks.sh fai2ban PPriv.py user"
 IVAR="/etc/http-instas"
